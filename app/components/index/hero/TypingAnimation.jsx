a1ca685d49c0cc06cfc5fe12bb93180a33e2e1f9
MaenAlamin <ma3n.93@gmail.com>
2024-04-03 00:08:26 +0300

    removed react-spring

--- app/components/index/hero/TypingAnimation.jsx
+++ app/components/index/hero/TypingAnimation.jsx
@@ -1,11 +1,12 @@
 import { Text } from "@chakra-ui/react";
 import { useState, useEffect, useRef } from "react";
-import { useTrail, animated } from "react-spring";
+import { motion, useAnimation } from "framer-motion";
 
 export const TypingAnimation = ({ words }) => {
   const [index, setIndex] = useState(0);
   const [word, setWord] = useState(words[index]);
   const wordRef = useRef(word); // Use a ref to store the current word
+  const controls = useAnimation(); // Framer Motion animation controls
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -13,30 +14,35 @@ export const TypingAnimation = ({ words }) => {
       const newWord = words[index];
       setWord(newWord);
       wordRef.current = newWord; // Update the ref with the new word
+      // Trigger the animation for the new word
+      controls.start({
+        opacity: [0, 1],
+        scale: [0, 1],
+        transition: {
+          duration: 0.5,
+          delay: (i) => i * 0.05, // Delay increases with each letter
+          staggerChildren: 0.05, // Optional: Adds a delay between each child animation
+        },
+      });
     }, 3000); // Change word every 3 seconds
     return () => clearTimeout(timer);
-  }, [index, words]);
-
-  // Use useTrail to create a staggered animation for each letter
-  const trail = useTrail(wordRef.current.length, {
-    from: { opacity: 0, transform: "scale(0)" },
-    to: { opacity: 1, transform: "scale(1)" },
-    config: { duration: 500 },
-    delay: 75, // Adjust delay as needed
-    reset: true, // Reset animation on word change
-    keys: wordRef.current, // Use the current word as a key to reset the animation
-  });
+  }, [index, words, controls]);
 
   return (
     <div
       style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
     >
-      {trail.map((animation, i) => (
-        <animated.div key={i} style={animation}>
+      {wordRef.current.split("").map((letter, i) => (
+        <motion.div
+          key={i}
+          custom={i}
+          animate={controls}
+          style={{ display: "inline-block" }}
+        >
           <Text as={"b"} fontSize={"8xl"} color={"white"}>
-            {wordRef.current[i]}
+            {letter}
           </Text>
-        </animated.div>
+        </motion.div>
       ))}
     </div>
   );

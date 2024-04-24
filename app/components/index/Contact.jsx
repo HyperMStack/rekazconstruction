83c0bd7d5825542415a8362ae26104ff758677d8
MaenAlamin <ma3n.93@gmail.com>
2024-04-24 11:48:09 +0300

    fixed footer links positioning

--- app/components/index/Contact.jsx
+++ app/components/index/Contact.jsx
@@ -81,7 +81,7 @@ export function Contact({ className }) {
                       />
                     </div>
                     <div>
-                      <button className="bg-gray-600 text-white px-5 py-2 rounded-none">
+                      <button className="bg-gray-600 text-white px-5 py-2 w-full md:w-auto rounded-none">
                         Send Message
                       </button>
                     </div>
--- app/components/layout/Footer.jsx
+++ app/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import { footerLinks, websiteData } from "../../data/data";
 
 export function Footer() {
   return (
-    <footer className="text-black md:text-white text-sm font-semibold relative my-auto mt-8 md:-mt-14">
+    <footer className="text-black md:text-white text-sm font-semibold relative my-auto mt-24 md:-mt-14">
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         <p className="text-center md:text-left text-sm w-full">
           &copy; 2023{" "}
